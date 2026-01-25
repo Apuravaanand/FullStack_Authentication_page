12b5ff92067fe11df369d12ea1aa6f70f974f571
Apurava Anand <erapuravaanand@gmail.com>
2026-01-25 21:47:40 +0530

    Adding render url

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,7 @@
 // frontend/services/authService.js
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "https://fullstack-authentication-page-3.onrender.com/";
 
 const api = axios.create({
   baseURL: `${API_URL}/api/auth`,
