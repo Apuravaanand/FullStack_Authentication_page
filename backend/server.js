81b2ab7889b464d4bfcd0b03e21a837d11437fd1
Apurava Anand <erapuravaanand@gmail.com>
2026-01-29 15:21:08 +0530

    Adding render url final2

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use(express.json());
 // CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL, // frontend dev server
+    origin: process.env.CLIENT_URL || "https://authentication-app-lap3.onrender.com", // frontend dev server
     credentials: true,
   })
 );
